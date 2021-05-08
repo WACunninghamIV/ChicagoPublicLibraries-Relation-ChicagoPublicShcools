503c3a510cfb342a0dff0c4358806800e12c0585
WorldClass13 <wacunninghamiv@gmail.com>
2021-05-08 10:33:37 -0500

    3rd Commit

---
+++ .ipynb_checkpoints/queries-checkpoint.sql
@@ -0,0 +1,17 @@
+-- Create tables for raw data to be loaded into
+CREATE TABLE libraries (
+ZIP INT PRIMARY KEY,
+NAME TEXT,
+);
+
+CREATE TABLE chicago_schools (
+Zip Code INT PRIMARY KEY,
+Name of School TEXT,
+);
+
+
+-- Joins tables
+SELECT libraries.ZIP, libraries.NAME, chicago_schools.ZIP, chicago_schools.Name of School
+FROM libraries
+JOIN chicago_schools
+ON libraries.ZIP = chicago_schools.ZIP;
